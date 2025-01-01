c2f154e090a2f89a3817a89d63a1eede5938eeca
Faiz Maula <fmaulaahmad@gmail.com>
2025-01-02 00:40:04 +0700

    banyak22

--- carimakanu_app/lib/pages/search.page.dart
+++ carimakanu_app/lib/pages/search.page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:carimakanu_app/pages/informasikedai.pages.dart';
-import 'package:flutter_svg/svg.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 import '../models/kedai.models.dart';
 
 class searchPage extends StatefulWidget {
   final String username;
   final String idUser;
-  const searchPage({Key? key, required this.username, required this.idUser});
+
+  const searchPage({Key? key, required this.username, required this.idUser}) : super(key: key);
 
   @override
   State<searchPage> createState() => _SearchPageState();
@@ -23,11 +23,11 @@ class _SearchPageState extends State<searchPage> {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
-        appBar: appBarWelcomePage(),
+        appBar: _appBarWelcomePage(),
         body: Column(
           children: [
             _searchField(),
-            const SizedBox(height: 10),
+            const SizedBox(height: 5),
             Expanded(
               child: StreamBuilder(
                 stream: FirebaseFirestore.instance
@@ -40,34 +40,145 @@ class _SearchPageState extends State<searchPage> {
                     return const Center(child: CircularProgressIndicator());
                   }
                   if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                    return const Center(child: Text('Tidak ada kedai ditemukan'));
-                  }
-                  return ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        // Konversi snapshot menjadi objek Kedai
-                        var kedai = Kedai.fromFirestore(snapshot.data!.docs[index]);
-                        return ListTile(
-                          title: Text(kedai.name), // Menggunakan kedai.name dari model Kedai
-                          subtitle: Text(kedai.alamat), // Menggunakan kedai.alamat dari model Kedai
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => informasiKedai(
-                                  kedai: kedai, // Mengirim objek Kedai
-                                  username: widget.username,
-                                  idUser: widget.idUser,
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, // Mengatur posisi teks di sisi kiri
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0), // Geser teks ke samping
+                          child: const Text(
+                            'Hasil Pencarian',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Lexend',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 5), // Jarak antara search bar dan kontainer
+                        Container(
+                          decoration: BoxDecoration(
+                            color: const Color(0xffF7F7F7),
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                blurRadius: 10,
+                                offset: const Offset(0, 4),
+                              ),
+                            ],
+                          ),
+                          width: 400,
+                          height: 354,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SvgPicture.asset(
+                                'assets/icons/Sad.svg',
+                                height: 130,
+                              ),
+                              const SizedBox(height: 16),
+                              const Text(
+                                'Kedai tidak ditemukan',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey,
+                                  fontFamily: 'Lexend',
                                 ),
                               ),
-                            );
-                          },
-                        );
-                      }
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+
+                  return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      // Konversi snapshot menjadi objek Kedai
+                      var kedai = Kedai.fromFirestore(snapshot.data!.docs[index]);
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => informasiKedai(
+                                kedai: kedai, // Mengirim objek Kedai
+                                username: widget.username,
+                                idUser: widget.idUser,
+                              ),
+                            ),
+                          );
+                        },
+
+                        child: Container(
+                          padding: const EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                blurRadius: 7,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Row(
+                            children: [
+                              // Gambar
+                              SizedBox(
+                                width: 100,
+                                height: 100,
+                                child: Image.asset(kedai.iconPath, fit: BoxFit.contain),
+                              ),
+                              const SizedBox(width: 8),
+                              // Detail Text
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      kedai.name,
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(kedai.kategori),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          '${kedai.rating} ★ | ',
+                                          style: const TextStyle(
+                                            color: Colors.orange,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          '${kedai.jumlahRating} ratings',
+                                          style: const TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 12,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Text(kedai.alamat, style: const TextStyle(fontSize: 12)),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
                   );
                 },
               ),
-            ),
+            )
           ],
         ),
       ),
@@ -103,8 +214,97 @@ class _SearchPageState extends State<searchPage> {
       ),
     );
   }
+  Widget _recommendedSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            'Recommended for you',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        SizedBox(
+          height: 220,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return Container(
+                width: 200,
+                margin: const EdgeInsets.only(left: 16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      blurRadius: 7,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15),
+                          ),
+                          child: Image.asset(
+                            'assets/images/sample_food.jpg',
+                            height: 120,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Positioned(
+                          top: 8,
+                          right: 8,
+                          child: Container(
+                            padding: const EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: const Icon(
+                              Icons.favorite_border,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Sate Lilit Bali',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
 
-  AppBar appBarWelcomePage() {
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  AppBar _appBarWelcomePage() {
     return AppBar(
       backgroundColor: Colors.white,
       toolbarHeight: 100.0,
@@ -132,4 +332,5 @@ class _SearchPageState extends State<searchPage> {
       ),
     );
   }
+
 }
